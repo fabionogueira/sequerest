c990951f555e319f82c10f2dcf097cb607670c89
fabionogueira <fabio.bacabal@gmail.com>
2018-03-08 16:33:30 -0300

    ...

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const api = require('./api');
+const db = require('./db');
+const server = require('./api');
+const auth = require('./auth');
+
+module.exports = {
+    api,
+    db,
+    server,
+    auth
+};
